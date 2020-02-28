78481d599091b2481449d28d1bf17662040aeaef
Vinh Pham <vinh.pham@spiritlabs.co>
2020-02-28 16:03:42 +0700

    Add styled component

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,16 +1,28 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
+import Loadable from "react-loadable";
+import styled from "styled-components";
 
 import Routes from "../routes";
-import Header from "./components/Header";
+
+const Header = Loadable({
+  loader: () => import("./components/Header"),
+  loading: () => <h1>Loading ...</h1>
+});
 
 const App = () => {
   return (
     <>
       <Header />
+      <Test>Test</Test>
       <Switch>
         {Routes.map((_route, index) => (
-          <Route key={index} path={_route.url} exact component={_route.component} />
+          <Route
+            key={index}
+            path={_route.url}
+            exact
+            component={_route.component}
+          />
         ))}
       </Switch>
     </>
@@ -18,3 +30,7 @@ const App = () => {
 };
 
 export default App;
+
+const Test = styled.div`
+  color: red;
+`;
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,29 +1,36 @@
 import React from "react";
 import Loadable from "react-loadable";
 
+// import Home from "./client/pages/Home";
+// import About from "./client/pages/About";
+// import Gallery from "./client/pages/Gallery";
+// import Typo from "./client/pages/Typo";
+// import Contact from "./client/pages/Contact";
+// import NotFound from "./client/pages/NotFound";
+
 const Home = Loadable({
   loader: () => import("./client/pages/Home"),
-  loading: () => <h1>Loadding ...</h1>
+  loading: () => <h1>Loading ...</h1>
 });
 const About = Loadable({
   loader: () => import("./client/pages/About"),
-  loading: () => <h1>Loadding ...</h1>
+  loading: () => <h1>Loading ...</h1>
 });
 const Gallery = Loadable({
   loader: () => import("./client/pages/Gallery"),
-  loading: () => <h1>Loadding ...</h1>
+  loading: () => <h1>Loading ...</h1>
 });
 const Typo = Loadable({
   loader: () => import("./client/pages/Typo"),
-  loading: () => <h1>Loadding ...</h1>
+  loading: () => <h1>Loading ...</h1>
 });
 const Contact = Loadable({
   loader: () => import("./client/pages/Contact"),
-  loading: () => <h1>Loadding ...</h1>
+  loading: () => <h1>Loading ...</h1>
 });
 const NotFound = Loadable({
   loader: () => import("./client/pages/NotFound"),
-  loading: () => <h1>Loadding ...</h1>
+  loading: () => <h1>Loading ...</h1>
 });
 
 const Routes = [
